e790a49e717be134e9678781f08b3e140ba513b6
frieder <friedrich.graefe@hs-augsburg.de>
2020-11-30 16:14:05 +0100

    alles pushen

--- run.py
+++ run.py
@@ -1,16 +1,31 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 
 
 
 app = Flask(__name__)
-print(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
 @app.route("/")
-def hello():
+def home():
     return render_template("start.html")
 
+@app.route("/gallery")
+def galley():
+    return render_template("gallery.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
+app.run(port=5000)
